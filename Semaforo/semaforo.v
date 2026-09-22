918d29be8c160bbe13f79e6543e52dea9a3cedb2
GuilhermeEsber <168978954+GuilhermeEsber@users.noreply.github.com>
2026-09-22 10:05:42 -0300

    Comment out semaforo module implementation
    Commented out the semaforo module and its logic.

--- Semaforo/semaforo.v
+++ Semaforo/semaforo.v
@@ -9,3 +9,26 @@ module semaforo (
     assign FD = ~A & ~B & ~C & D;
 
 endmodule
+
+// module semaforo (
+//     input A, B, C, D,
+//     output reg FA, FB, FC, FD
+// );
+
+//     always @(*) begin
+//         FA = 0;
+//         FB = 0;
+//         FC = 0;
+//         FD = 0;
+
+//         if (A)
+//             FA = 1;
+//         else if (B)
+//             FB = 1;
+//         else if (C)
+//             FC = 1;
+//         else if (D)
+//             FD = 1;
+//     end
+
+// endmodule
